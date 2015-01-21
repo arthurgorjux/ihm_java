3a25302beae66edf10d7c77399053e619443bc87
nicolasyong <nicolas_yong@hotmail.com>
2015-01-21 17:18:12 +0100

    caca

---
+++ src/AvancerNormalListener.java
@@ -0,0 +1,60 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nicolas
+ */
+public class AvancerNormalListener implements ActionListener{
+    
+    private JButton next;
+    private GalleryPanel p;
+    
+    public AvancerNormalListener(JButton next, GalleryPanel aThis){
+        this.next = next;
+        this.p = aThis;
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+         switch (p.etat){
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+                break;
+            case 4 :
+                p.etat = 4;
+                // continuer à faire défiler les images
+                break;
+            case 5 :
+            case 6 :
+            case 7 :
+                break;
+            case 8 :
+                p.etat = 8;
+                // continuer à faire défiler les images
+                break;
+            case 9 :
+                p.etat = 9;
+                // continuer à faire défiler les images
+                break;
+            case 10 :
+            case 11 :
+            case 12 :
+            case 13 :
+            case 14 :
+                break;               
+        }
+    }
+    
+}
