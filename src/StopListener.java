07099354334e6d32142c6917a4f8cc3ed2050f56
nicolasyong <nicolas_yong@hotmail.com>
2015-01-22 18:31:11 +0100

    rajout des listeners

---
+++ src/StopListener.java
@@ -0,0 +1,32 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nicolas
+ */
+public class StopListener implements ActionListener{
+
+    private JButton stop;
+    private GalleryPanel p;
+
+    public StopListener(JButton stop, GalleryPanel aThis){
+        this.stop = stop;
+        this.p = aThis;
+        
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
